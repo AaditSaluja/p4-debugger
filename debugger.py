e85ca9b82d43fbf04c55e70dff862347e31976c0
AaditSaluja <aaditsaluja@gmail.com>
2023-05-05 12:32:44 -0700

    Updated Register Parser to handle large files

--- debugger.py
+++ debugger.py
@@ -1,4 +1,5 @@
 def parse_registerf(content, reg_handle):
+    # data in the format index : [val, changecounter]
     reg_data = {}
     check_str = 'Wrote register \'' + reg_handle.strip() + '\' '
     output_file = open("out.txt", "w")
@@ -6,15 +7,53 @@ def parse_registerf(content, reg_handle):
     for i in content:
         debug.write(i)
         if(check_str in i):
+            
             k = i.split(check_str + 'at index ')
             key_val = k[1].split()
-            reg_data[key_val[0]] = key_val[3]
+            try:
+                counter = reg_data[key_val[0]][1]
+            except:
+                counter = 0
+            reg_data[key_val[0]] = [key_val[3], counter + 1]
     
     for key, val in reg_data.items():
-        output_file.write(key + ": " + val + "\n")
+        output_file.write(key + ": " + val[0] + " - " + val[1] + "\n")
 
     output_file.close()
     debug.close()
+
+def parse_register_largef(file_handler, reg_handle, final_time_lst):
+    # data in the format: index : [val, changecounter]
+    reg_data = {}
+    check_str = 'Wrote register \'' + reg_handle.strip() + '\' '
+    output_file = open("out.txt", "w")
+    debug= open("out2.txt", "w")
+
+    while(True):
+        try:
+            k = file_handler.readline()
+            if(k[0] == "[" and (int(k[1:3]) > final_time_lst[0] or (int(k[1:3]) ==  final_time_lst[0] and int(k[4:6]) > final_time_lst[1]) or ( (int(k[1:3]) == final_time_lst[0] and int(k[4:6]) == final_time_lst[1]) and float(k[7:13]) > final_time_lst[1]))):
+                print("Reached Limit")
+                break
+            if(check_str in k):
+                debug.write(k)
+                line_sp = k.split(check_str + 'at index ')
+                key_val = line_sp[1].split()
+                try:
+                    counter = reg_data[key_val[0]][1]
+                except:
+                    counter = 0
+                reg_data[key_val[0]] = [key_val[3], counter + 1]
+        except:
+            break
+    
+    for key, val in reg_data.items():
+        output_file.write(key + ": " + val[0] + " - " + str(val[1]) + "\n")
+
+    output_file.close()
+    debug.close()
+
+
 
 
 
@@ -59,9 +98,13 @@ while True:
                 break
             
             content = []
+            call_type = 0 # 0 for small files, 1 for large files/ timeperiod specified
+            final_time_lst = 0
             if(timeperiod == 0):
                 content = file.read()
+                call_type = 0
             else:
+                call_type = 1
                 while(True):
                     try:
                         k = file.readline()
@@ -90,23 +133,18 @@ while True:
                 
                 
 
-                while(True):
-                    try:
-                        k = file.readline()
-                        if(k[0] == "[" and (int(k[1:3]) > final_time_lst[0] or (int(k[1:3]) ==  final_time_lst[0] and int(k[4:6]) > final_time_lst[1]) or ( (int(k[1:3]) == final_time_lst[0] and int(k[4:6]) == final_time_lst[1]) and float(k[7:13]) > final_time_lst[1]))):
-                            print(final_time_lst)
-                            print(k[1:3], k[4:6], k[7:13])
-                            print("Reached Limit")
-                            break
-                        content.append(k)
-                    except:
-                        break
 
-            file.close()
+
+
             control_name = input("Name of the Control Process with Register (Typically MyIngress): ")
             reg_name = input("Name of Register to Track: ")
             reg_handle = control_name + '.' + reg_name
-            parse_registerf(content, reg_handle)
+            if(call_type == 1):
+                parse_register_largef(file, reg_handle, final_time_lst)
+                file.close()
+            else:
+                file.close()
+                parse_registerf(content, reg_handle)
 
     elif(command == "2"):
         track_pathf(0, 0, 0)
