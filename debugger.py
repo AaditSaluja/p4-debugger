0440ee7c720d6cc5c9864293e6f04db3cbffc1c5
AaditSaluja <aaditsaluja@gmail.com>
2023-05-05 11:43:55 -0700

    Parsing Registers Completed

--- debugger.py
+++ debugger.py
@@ -1,5 +1,22 @@
-def parse_registerf(filehandler, timeperiod, registername):
-    return 0
+def parse_registerf(content, reg_handle):
+    reg_data = {}
+    check_str = 'Wrote register \'' + reg_handle.strip() + '\' '
+    output_file = open("out.txt", "w")
+    debug= open("out2.txt", "w")
+    for i in content:
+        debug.write(i)
+        if(check_str in i):
+            k = i.split(check_str + 'at index ')
+            key_val = k[1].split()
+            reg_data[key_val[0]] = key_val[3]
+    
+    for key, val in reg_data.items():
+        output_file.write(key + ": " + val + "\n")
+
+    output_file.close()
+    debug.close()
+
+
 
 def track_pathf(filehandler, timeperiod, registername):
     return 0
@@ -66,32 +83,31 @@ while True:
                             
                             # time at which we need to stop
                             final_time_lst = [int(ini_time[0]), int(ini_time[1]), float(round(ini_time[2],3))]
-                            
-                            print(final_time_lst)
-
                             break
                     except:
                         print("Invalid File Type or Empty File")
                         break
                 
+                
 
                 while(True):
-                    print("hello")
                     try:
                         k = file.readline()
-                        print(k)
-                        if(k[0] == "[" and (int(k[1:3]) > final_time_lst[0] or int(k[4:6]) > final_time_lst[1] or (int(k[4:6]) == final_time_lst[1] and float(k[7:13]) > final_time_lst[1]))):
+                        if(k[0] == "[" and (int(k[1:3]) > final_time_lst[0] or (int(k[1:3]) ==  final_time_lst[0] and int(k[4:6]) > final_time_lst[1]) or ( (int(k[1:3]) == final_time_lst[0] and int(k[4:6]) == final_time_lst[1]) and float(k[7:13]) > final_time_lst[1]))):
+                            print(final_time_lst)
+                            print(k[1:3], k[4:6], k[7:13])
                             print("Reached Limit")
                             break
                         content.append(k)
                     except:
-                        print("hello2")
-                        # print(content)
                         break
-                    
 
-                file.close()
-        parse_registerf(0, 0, 0)
+            file.close()
+            control_name = input("Name of the Control Process with Register (Typically MyIngress): ")
+            reg_name = input("Name of Register to Track: ")
+            reg_handle = control_name + '.' + reg_name
+            parse_registerf(content, reg_handle)
+
     elif(command == "2"):
         track_pathf(0, 0, 0)
     elif(command == "3"):
